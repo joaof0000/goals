3419ccb163c2c4b4dd42e8727c06573a18d6921c
joaof0000 <joaoifilipe@gmail.com>
2023-07-11 10:45:07 -0400

    login

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,27 @@ const passport = require('passport');
 
 // The root route renders our only view
 router.get('/', function(req, res) {
+  res.render('index')
+})
+
+// Google OAuth login route
+router.get(
+  '/auth/google',
+  passport.authenticate(
+    'google',
+    { scope: ['profile', 'email'], prompt: 'select_account' } // prompt is optional
+  )
+)
+  
   //UPDATE THIS
+
   // Where do you want to go for the root route
   // in the student demo this was res.redirect('/movies'), what do you want?
   // This could be a landing page, or just redirect to your main resource page which you'll have an a tag that makes 
   // a request to `/auth/google` route below
-});
+
+
+
 
 // Google OAuth login route
 router.get('/auth/google', passport.authenticate(
@@ -20,8 +35,8 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : '/', // UPDATE THIS, where do you want the client to go after you login 
-    failureRedirect : '/' //  UPDATE THIS, where do you want the client to go if login fails
+    successRedirect : '/goals', // UPDATE THIS, where do you want the client to go after you login 
+    failureRedirect : '/goals' //  UPDATE THIS, where do you want the client to go if login fails
   }
 ));
 
@@ -30,6 +45,6 @@ router.get('/logout', function(req, res){
   req.logout(function(){ //< - req.logout comes from passport, and what it does is destorys the cookie keeping track of the user!
     res.redirect('/'). // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
   })
-})
+});
 
 module.exports = router;
